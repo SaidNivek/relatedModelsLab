c4cabcbe8884cb6c1e5be2f6656c73b99d5e34e5
SaidNivek <kevin.vi.dias@gmail.com>
2022-04-25 15:24:51 -0400

    Change to singers/songs in song_controller

--- controllers/song_controller.js
+++ controllers/song_controller.js
@@ -13,8 +13,8 @@ const db = require('../models')
 router.get('/', async (req,res, next)=>{
     // res.send('hitting review index')
     try {
-        const allReviews = await db.Review.find({})
-        res.send(allReviews)
+        const allSongs = await db.Song.find({})
+        res.send(allSongs)
 
     }catch(error){
         console.log(error);
@@ -27,9 +27,9 @@ router.get('/', async (req,res, next)=>{
 router.get('/new', async (req,res, next)=>{
 
     try {
-        const allProducts = await db.Product.find({})
+        const allSongs = await db.Song.find({})
         // console.log(allProducts)
-        const context ={products: allProducts}
+        const context ={songs: allSongs}
         res.render('reviews/new.ejs', context)
 
     }catch(error){
@@ -42,11 +42,11 @@ router.get('/new', async (req,res, next)=>{
 // create - POST route 
 router.post('/', async (req,res, next)=>{
     try{
-        const newReviewData = req.body
-        const newReview = await db.Review.create(newReviewData)
-        console.log(newReview)
-        // res.redirect('/reviews')
-        res.redirect(`/products/${newReview.product}`)
+        const newRSongData = req.body
+        const newSong = await db.Song.create(newSongData)
+        console.log(newSong)
+        // res.redirect('/Songs')
+        res.redirect(`/singers/${newSong.product}`)
         // return user to product detail page -> 
     }catch(error){
         console.log(error);
@@ -55,11 +55,11 @@ router.post('/', async (req,res, next)=>{
     }
 })
 // show - serve a show.ejs template
-router.get('/:reviewId', async (req,res, next)=>{
+router.get('/:songId', async (req,res, next)=>{
     // res.send('hitting review show: '+req.params.reviewId)
     try {
-        const foundReview = await db.Review.findById(req.params.reviewId).populate('product')
-        res.render('reviews/show.ejs', {review: foundReview})
+        const foundSong = await db.Song.findById(req.params.songId).populate('singer')
+        res.render('reviews/show.ejs', {song: foundSong})
     }catch(error){
         console.log(error);
         req.error = error;
@@ -67,20 +67,20 @@ router.get('/:reviewId', async (req,res, next)=>{
     }
 })
 // update - PUT route
-router.put('/:reviewId', async (req,res, next)=>{
-    res.send('hitting review update: '+req.params.reviewId)
+router.put('/:songId', async (req,res, next)=>{
+    res.send('hitting song update: '+req.params.songId)
 })
 // edit - GET - serve an edit.ejs
-router.get('/:reviewId/edit', async (req,res, next)=>{
-    res.send('hitting review edit: '+req.params.reviewId)
+router.get('/:songId/edit', async (req,res, next)=>{
+    res.send('hitting song edit: '+req.params.songId)
 })
 // destroy - delete 
-router.delete('/:reviewId', async (req,res, next)=>{
+router.delete('/:songId', async (req,res, next)=>{
     // res.send('hitting review delete: '+req.params.reviewId)
     try{
-       const deleteReview = await db.Review.findByIdAndDelete(req.params.reviewId)
-       console.log(deleteReview.id, "<<review |",deleteReview.product,"<<product") 
-       res.redirect('/products/'+deleteReview.product)
+       const deleteSong = await db.Song.findByIdAndDelete(req.params.songId)
+       console.log(deleteSong.id, "<<Song |",deleteSong.product,"<<product") 
+       res.redirect('/singers/'+deleteSong.product)
     }catch(error){
         console.log(error);
         req.error = error;
